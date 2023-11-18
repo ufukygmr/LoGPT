085f6944dd861c4c9f9d11ec324e65c5dcd46e86
Burak Durak <burak.durak@carealytix.com>
2023-11-18 12:28:50 +0100

    message bubble added

---
+++ logpt-mobile/src/components/message-box.tsx
@@ -0,0 +1,71 @@
+import { Box, Text, View } from "native-base";
+import { ColorType } from "native-base/lib/typescript/components/types";
+import React, { ReactNode, useMemo } from "react";
+import { colors } from "../lib/colors";
+import { ViewStyle } from "react-native";
+
+export interface MessageBoxProps {
+  message: string;
+  incoming?: boolean;
+}
+
+interface BubbleIndicatorProps {
+  backgroundColor: ColorType;
+  incoming: boolean;
+}
+function BubbleIndicator({ backgroundColor, incoming }: BubbleIndicatorProps) {
+  const bubbleStyle: ViewStyle = incoming ? { left: 0 } : { right: 0 };
+  const indicatorStyle: ViewStyle = incoming
+    ? {
+        right: 0,
+        borderBottomRightRadius: 100,
+      }
+    : {
+        left: 0,
+        borderBottomLeftRadius: 100,
+      };
+  return (
+    <View
+      position={"absolute"}
+      bg={backgroundColor}
+      zIndex={100}
+      bottom={0}
+      width={4}
+      height={4}
+      style={bubbleStyle}>
+      <Box
+        position={"absolute"}
+        bottom={0}
+        width={8}
+        height={8}
+        bg={colors.background}
+        style={indicatorStyle}></Box>
+    </View>
+  );
+}
+
+export function MessageBox({ message, incoming = false }: MessageBoxProps) {
+  const backgroundColor: ColorType = useMemo(
+    () => colors.message[incoming ? "incoming" : "outgoing"].background,
+    [incoming]
+  );
+  return (
+    <View alignSelf={incoming ? "flex-start" : "flex-end"} px={4}>
+      <Box
+        bg={backgroundColor}
+        px={4}
+        py={2}
+        borderRadius={8}
+        borderBottomRightRadius={incoming ? 8 : 0}
+        borderBottomLeftRadius={incoming ? 0 : 8}>
+        <Text
+          color={colors.message[incoming ? "incoming" : "outgoing"].text}
+          fontWeight={500}
+          fontSize={"16px"}>
+          {message}
+        </Text>
+      </Box>
+      <BubbleIndicator backgroundColor={backgroundColor} incoming={incoming} />
+    </View>
+  );
+}
---
+++ logpt-mobile/src/lib/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  background: "#2e4057",
+  message: {
+    outgoing: {
+      background: "#f1ecce",
+      text: "#0090c1",
+    },
+    incoming: {
+      background: "#38aecc",
+      text: "#f4ebd9",
+    },
+  },
+};
