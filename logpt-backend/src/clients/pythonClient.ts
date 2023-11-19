6c18d3141c0eb764008f53a8db057774d4a99973
Osman Yagmur <ge85vej@mytum.de>
2023-11-19 05:33:38 +0100

    finished backend

--- logpt-backend/src/clients/pythonClient.ts
+++ logpt-backend/src/clients/pythonClient.ts
@@ -1,12 +1,12 @@
 import { spawn } from 'child_process';
 
-const pythonScript = '../x.py'; //TODO: Handle this path
+const pythonScript = '/Users/ufukyagmur/Desktop/LoGPT/text-similarity/measure_similarity.py'; //TODO: Handle this path
 
-export const runPythonScript = (args: string[]) => {
+export const runPythonScript = (args: string[]): Promise<string> => {
   return new Promise((resolve, reject) => {
     // Spawn a new Python process
-    const pythonProcess = spawn('python', [pythonScript, ...args]);
-
+    const pythonProcess = spawn('python3', [pythonScript, ...args]);
+    console.log(args);
     let stdoutData = '';
 
     // Capture the output of the Python script
